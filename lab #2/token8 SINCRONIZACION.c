58d18a3879c91d07e7f7c2b57b3f95126335c1a0
Cristian Mora <cristianandres2014@outlook.es>
2025-02-23 23:40:27 -0500

    twice

--- lab #2/token8 SINCRONIZACION.c
+++ lab #2/token8 SINCRONIZACION.c
@@ -36,16 +36,14 @@ int main() {
     // Calcula el tiempo transcurrido en milisegundos
     tiempo = ((stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec) / 1000;
     // Calcula el número de intervalos de 30 segundos (30000 ms)
-    // Para tiempos menores a 30000 ms, interval será 0; para 30000-59999 ms, 1; etc.
     unsigned long interval = tiempo / 30000;
-    // Calcula el operando a usar: 0, 30000, 60000, etc.
    // unsigned long operando = interval * 30;
  
 unsigned long operando=0; // LO NECESITO EN 0 PARA TOKEN LOCAL FIRST OPTION = PASSWORD
 unsigned long i;
 
 for (i = 1; i <= interval; i++) {
-    operando = operando + 300; // Multiplica el operando por 30 en cada iteración
+    operando = operando + 30; // Multiplica el operando por 30 en cada iteración
 }
 	
     // Si operando es 0 (primer intervalo), el token es la misma clave.
@@ -55,7 +53,7 @@ for (i = 1; i <= interval; i++) {
     	
         printf("Token correcto. ¡Acceso permitido!\n");
         printf("Tiempo transcurrido: %lu ms\n", tiempo);
-        printf("Intervalo (cada 30s): %lu\n", interval);
+  //      printf("Intervalo (cada 30s): %lu\n", interval);
         printf("Clave %08lX\n", clave);
         printf("Token Local %08lX\n", tokenLocal);
 
@@ -63,17 +61,17 @@ for (i = 1; i <= interval; i++) {
         printf("\n\nAHORA PRESIONE ENTER PARA TERMINAR PROGRAMA\n\n", tokenLocal);        
         getchar();
         return 0;
+        
     } else {
         printf("Token incorrecto. ¡Acceso denegado!\n");
         printf("Tiempo: %lu ms\n", tiempo);
-        printf("Intervalo (cada 30s): %lu\n", interval);
+   //     printf("Intervalo (cada 30s): %lu\n", interval);
         printf("Clave %08lX\n", clave);
         printf("Token Local %08lX\n", tokenLocal);
         tokenIngresado=0;
         system("pause");
     }
-   
-    	
-	}
+     	
+  }
 
 }
